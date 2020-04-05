58e6ceab9c575415892086d495de685e896ac695
vikas.dhongade <vikas.dhongade@AVN-MKTG-LAP-010.avindia.com>
2020-04-05 14:18:17 +0530

    Added TaskB class which implements runnable interface

--- ThreadDemo/src/mypack/thread/ThreadAndRunnable.java
+++ ThreadDemo/src/mypack/thread/ThreadAndRunnable.java
@@ -11,6 +11,18 @@ class TaskA extends Thread{
 	
 }
 
+class TaskB implements Runnable{
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("TaskB Running Thread Name:: -> "+Thread.currentThread().getName());
+	}
+	
+	
+}
+
 public class ThreadAndRunnable {
 
 	public static void main(String[] args) {
@@ -21,6 +33,13 @@ public class ThreadAndRunnable {
 			TaskA ta=new TaskA();
 			ta.setName("TaskA"+i);
 			ta.start();
+		}
+		
+		for(int i=1;i<=5;i++) {
+			
+			Thread t=new Thread(new TaskB());
+			t.setName("TaskB"+i);
+			t.start();
 		}
 	}
 
