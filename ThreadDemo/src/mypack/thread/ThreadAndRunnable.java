b88077a87073402be5beb3ea97bea7fc4284feff
vikas.dhongade <vikas.dhongade@AVN-MKTG-LAP-010.avindia.com>
2020-04-28 11:14:08 +0530

    Removed Comments

--- ThreadDemo/src/mypack/thread/ThreadAndRunnable.java
+++ ThreadDemo/src/mypack/thread/ThreadAndRunnable.java
@@ -1,46 +1,44 @@
 package mypack.thread;
 
-class TaskA extends Thread{
+class TaskA extends Thread {
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		System.out.println("TaskA Running Thread Name:: -> "+Thread.currentThread().getName());
+		
+		System.out.println("TaskA Running Thread Name:: -> " + Thread.currentThread().getName());
 	}
-	
-	
+
 }
 
-class TaskB implements Runnable{
+class TaskB implements Runnable {
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
 		
-		System.out.println("TaskB Running Thread Name:: -> "+Thread.currentThread().getName());
+
+		System.out.println("TaskB Running Thread Name:: -> " + Thread.currentThread().getName());
 	}
-	
-	
+
 }
 
 public class ThreadAndRunnable {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		for(int i=1;i<=5;i++) {
-			
-			TaskA ta=new TaskA();
-			ta.setName("TaskA"+i);
+
+		for (int i = 1; i <= 5; i++) {
+
+			TaskA ta = new TaskA();
+			ta.setName("TaskA" + i);
 			ta.start();
 		}
-		
-		for(int i=1;i<=5;i++) {
-			
-			Thread t=new Thread(new TaskB());
-			t.setName("TaskB"+i);
+
+		for (int i = 1; i <= 5; i++) {
+
+			Thread t = new Thread(new TaskB());
+			t.setName("TaskB" + i);
 			t.start();
 		}
+
 	}
 
 }
